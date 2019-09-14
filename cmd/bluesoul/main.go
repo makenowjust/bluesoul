bc2861e480b80d00a6849caa88e931d7e0f15c76
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-09-14 15:10:10 +0900

    fix: support Go 1.13

--- cmd/bluesoul/main.go
+++ cmd/bluesoul/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/MakeNowJust/bluesoul"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
+
+// `-unsafeptr` flag is given when it is used via `go vet -vettool`.
+// But the flag is not defined in singlechecker for now.
+//
+// See https://github.com/golang/go/issues/34053
+var _ = flag.Bool("unsafeptr", false, "(for Go 1.13 compatibilit)")
 
 func main() { singlechecker.Main(bluesoul.Analyzer) }
